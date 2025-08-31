7f1f698b46d61762ab04e6133f603804e4f5f757
Rafael Torraca <rafael.torraca@icloud.com>
2025-08-31 00:11:04 -0300

    Day 1

---
+++ 001_print/hello.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+print( "Hello, World!\nHello, World!\nHello, World!")
+print("""
+Hello, World!
+Hello, World!
+Hello, World!
+""")
+
+print("Hello,", "Rafael")
+print("Hello," + " " + "Rafael")
---
+++ 001_print/input.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+length = len(name)
+
+print("Hello, " + name + "!")
+print("Hello,", name + "!")
+print(f"Hello, {name}! {length} characters long.")
+
+print("Hello, " + input("Enter your name: ") + "!")
+
+print(len(name))
